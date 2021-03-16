393f21c265f09992335a7427698b0b640895f55b
Ignacio Sanmartino <nachosanmartino@gmail.com>
2021-03-16 18:50:19 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  
+  const [ objetivoAgregado, setObjetivoAgregado] = useState('');
+  const [ objetivosAgregados, setObjetivosAgregados ] = useState([]);
+
+  const objetivoHandler = (objetivoText) => {
+    setObjetivoAgregado(objetivoText);
+  }
+
+  const addObjetivoHandler = () => {
+    setObjetivosAgregados(objetivosActuales => [...objetivosActuales, objetivoAgregado])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.layout}>
+      <View style={styles.formGroup}>
+        <TextInput 
+          placeholder="Tu objetivo"
+          style={styles.inputText} 
+          onChangeText={objetivoHandler}
+          value={objetivoAgregado}
+        />
+        <Button 
+          title="Agregar" 
+          onPress={addObjetivoHandler}
+        />
+      </View>
+      <View>
+        {objetivosAgregados.map((objetivo) => 
+          <View style={styles.listItem}>
+            <Text key={objetivo}>{objetivo}</Text>
+          </View>
+        )}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  layout: {
+    padding: 50
   },
+  formGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  inputText: {
+    width: '70%',
+    borderBottomColor: '#DCDCDC', 
+    borderBottomWidth: 1,
+    padding: 5
+  },
+  listItem: {
+    padding: 15,
+    backgroundColor: '#DCDCDC',
+    borderColor: 'black',
+    borderWidth: 1,
+    marginBottom: 10
+  }
 });
